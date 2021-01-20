e0fb066686485e53c5904084c4131e2f10c1efdf
andrewskylark <andrew.skylark@gmail.com>
2021-01-20 11:19:53 +0400

    добавляет шапку сайта, навигацию и мобильную навигацию со скриптом и без

---
+++ build/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+(() => {
+const nav = document.querySelector(`.nav`);
+const navToggle = document.querySelector(`.nav__toggle`);
+const logo = document.querySelector(`.page-header__logo`);
+
+logo.classList.remove(`page-header__logo--no-js`);
+nav.classList.remove(`nav--no-js`);
+
+navToggle.addEventListener(`click`, () => {
+
+  if (nav.classList.contains(`nav--closed`)) {
+    nav.classList.remove(`nav--closed`);
+    nav.classList.add(`nav--opened`);
+  } else {
+    nav.classList.add(`nav--closed`);
+    nav.classList.remove(`nav--opened`);
+  }
+});
+})();
