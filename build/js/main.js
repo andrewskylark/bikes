1fc6a63c047250dc9a6bad3c0f9276f60dd35f84
andrewskylark <andrew.skylark@gmail.com>
2021-01-21 20:08:37 +0400

    добавляет блоки видов велосипедов и преимущества

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,20 @@
 'use strict';
 (() => {
-const nav = document.querySelector(`.nav`);
-const navToggle = document.querySelector(`.nav__toggle`);
-const logo = document.querySelector(`.page-header__logo`);
+  const nav = document.querySelector(`.nav`);
+  const navToggle = document.querySelector(`.nav__toggle`);
+  const logo = document.querySelector(`.page-header__logo`);
 
-logo.classList.remove(`page-header__logo--no-js`);
-nav.classList.remove(`nav--no-js`);
+  logo.classList.remove(`page-header__logo--no-js`);
+  nav.classList.remove(`nav--no-js`);
 
-navToggle.addEventListener(`click`, () => {
+  navToggle.addEventListener(`click`, () => {
 
-  if (nav.classList.contains(`nav--closed`)) {
-    nav.classList.remove(`nav--closed`);
-    nav.classList.add(`nav--opened`);
-  } else {
-    nav.classList.add(`nav--closed`);
-    nav.classList.remove(`nav--opened`);
-  }
-});
+    if (nav.classList.contains(`nav--closed`)) {
+      nav.classList.remove(`nav--closed`);
+      nav.classList.add(`nav--opened`);
+    } else {
+      nav.classList.add(`nav--closed`);
+      nav.classList.remove(`nav--opened`);
+    }
+  });
 })();
